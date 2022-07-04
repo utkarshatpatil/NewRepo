c6535a2b58b7252de43b2d60cbd26f05c9b6ce40
utkarshatpatil <utkarshatpatil@gmail.com>
2022-07-04 15:02:53 +0530

    first commit

---
+++ Demo/src/test/Calculator.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Calculator {
+	
+	private int a,b,sum;
+
+	public void add()
+	{
+		sum =a+b;
+		System.out.println("Addition :"+sum);
+	}
+	
+	public int multiply(int a,int b)
+	{
+		return a*b;
+	}
+}
---
+++ Demo/src/test/CalculatorTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator c1=new Calculator();
+		c1.add();
+		System.out.println("Multiplication :"+c1.multiply(2, 6));
+	}
+
+}
