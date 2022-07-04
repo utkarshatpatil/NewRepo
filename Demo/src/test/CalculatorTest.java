6848b9be2fb71bbefaa7490111926ee2e4512f5b
utkarshatpatil <utkarshatpatil@gmail.com>
2022-07-04 16:09:24 +0530

    Changes made by Tester

--- Demo/src/test/CalculatorTest.java
+++ Demo/src/test/CalculatorTest.java
@@ -1,6 +1,14 @@
 package test;
 
 public class CalculatorTest {
+	
+	public void addTest()
+	{
+		int a = 5;
+		int b = 6;
+		int sum = a+b;
+		System.out.println("Addition = "+sum);
+	}
 
 	public static void main(String[] args) {
 		Calculator c1=new Calculator();
